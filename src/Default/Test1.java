41f4b9b693ab357b3768383c89b6a5b17e070fad
admin <admin@WINCTRL-7M9H3FQ>
2020-07-23 13:08:45 +0530

    This is my First Commit..

---
+++ src/Default/Test1.java
@@ -0,0 +1,14 @@
+package Default;
+
+public class Test1 {
+
+	public static void Git1() {
+		System.out.println("This is Git demo-1");
+	}
+	
+	public static void main(String[] args) {
+		Git1();
+	}
+	
+	
+}
